f4b100802737eef4cdd2ade7662fa371e7f49a2e
hooman <hoomanhajrian@gmail.com>
2025-03-11 19:43:19 -0700

    signin component changes

--- src/app/components/auth/SignInForm.tsx
+++ src/app/components/auth/SignInForm.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { useState, useEffect } from 'react';
-import { signIn, useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { useState, useEffect } from "react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import { Role } from '@/app/types';
+import { Role } from "@/app/types";
 
 const signInFormSchema = z.object({
   email: z.string().email("Invalid email address"),
@@ -35,11 +35,8 @@ const SignInForm: React.FC<SignInFormProps> = ({ onClose }) => {
     setLoading(true);
     setError(null);
 
-    console.log(data);
-    
-
     try {
-      const result = await signIn('credentials', {
+      const result = await signIn("credentials", {
         email: data.email,
         password: data.password,
         redirect: false, // Prevent automatic redirection
@@ -55,7 +52,7 @@ const SignInForm: React.FC<SignInFormProps> = ({ onClose }) => {
         setError("An unknown error occurred during sign-in.");
       }
     } catch (err: any) {
-      setError(err.message || 'An unexpected error occurred');
+      setError(err.message || "An unexpected error occurred");
       setLoading(false);
     }
   };
@@ -63,14 +60,20 @@ const SignInForm: React.FC<SignInFormProps> = ({ onClose }) => {
   useEffect(() => {
     if (status === "authenticated" && session?.user) {
       // Redirect based on user role
-      if (session.user.role === Role.ADMIN || session.user.role === Role.EDITOR) {
-        router.push('/admin');
-      } else if (session.user.role === Role.CLIENT || session.user.role === Role.PRINTER) {
-        router.push('/main');
+      if (
+        session.user.role === Role.ADMIN ||
+        session.user.role === Role.EDITOR
+      ) {
+        router.push("/admin");
+      } else if (
+        session.user.role === Role.CLIENT ||
+        session.user.role === Role.PRINTER
+      ) {
+        router.push("/main");
       } else {
-        router.push('/'); // Default redirect
+        router.push("/"); // Default redirect
       }
-    } else if (status === "unauthenticated" && loading === false){
+    } else if (status === "unauthenticated" && loading === false) {
       // if the user is unauthenticated after loading is false, then there was an issue with sign in.
       // the error should be displayed above the form.
     }
@@ -78,47 +81,62 @@ const SignInForm: React.FC<SignInFormProps> = ({ onClose }) => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-  <div className="mb-4">
-    <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email</label>
-    <input
-      type="email"
-      id="email"
-      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-      {...register("email")}
-      placeholder="Email"
-      required
-    />
-    {errors.email && (
-      <p className="text-red-500 text-xs italic">{errors.email?.message}</p>
-    )}
-  </div>
-  <div className="mb-6">
-    <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-    <input
-      type="password"
-      id="password"
-      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-      {...register("password")}
-      placeholder="Password"
-      required
-    />
-    {errors.password && (
-      <p className="text-red-500 text-xs italic">{errors.password?.message}</p>
-    )}
-  </div>
-  <div className="flex items-center justify-between">
-    <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      type="submit"
-      disabled={loading}
-    >
-      {loading ? 'Logging in...' : 'Sign In'}
-    </button>
-    <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-      Forgot Password?
-    </a>
-  </div>
-</form>
+      <div className="mb-4">
+        <label
+          htmlFor="email"
+          className="block text-gray-700 text-sm font-bold mb-2"
+        >
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          {...register("email")}
+          placeholder="Email"
+          required
+        />
+        {errors.email && (
+          <p className="text-red-500 text-xs italic">{errors.email?.message}</p>
+        )}
+      </div>
+      <div className="mb-6">
+        <label
+          htmlFor="password"
+          className="block text-gray-700 text-sm font-bold mb-2"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          {...register("password")}
+          placeholder="Password"
+          required
+        />
+        {errors.password && (
+          <p className="text-red-500 text-xs italic">
+            {errors.password?.message}
+          </p>
+        )}
+      </div>
+      <div className="flex items-center justify-between">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+          disabled={loading}
+        >
+          {loading ? "Logging in..." : "Sign In"}
+        </button>
+        <a
+          className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+          href="#"
+        >
+          Forgot Password?
+        </a>
+      </div>
+    </form>
   );
 };
 
