71bbffbc7a73d39557af393acc72f70e17b1f6cb
hooman <hoomanhajrian@gmail.com>
2025-03-16 23:17:33 -0700

    login card Alert and Alert components

--- src/app/components/notification/Alert.tsx
+++ src/app/components/notification/Alert.tsx
@@ -1,71 +1,50 @@
 "use client";
 import { FC, useEffect, useState } from "react";
+import { Alert, Stack } from "@mui/material";
+import { set } from "react-hook-form";
 interface AlertProps {
   status: "success" | "warning" | "error" | "info";
   message: string;
 }
 
-export const Alert: FC<AlertProps> = ({ message, status }) => {
-  const [show, setShow] = useState(false);
+const AlertComponent: FC<AlertProps> = ({
+  message = "Message Box",
+  status = "error",
+}) => {
+  const [open, setOpen] = useState(false);
 
-  const showAlert = () => {
+  const handleClose = () => {
+    setOpen(true);
     setTimeout(() => {
-      setShow(false);
+      setOpen(false);
     }, 5000);
   };
 
   useEffect(() => {
-    showAlert();
-    return () => {
-      setShow(false);
-    };
+    handleClose();
   }, []);
-  switch (status) {
-    case "success":
-      return (
-        <div className={show ? "block" : "hidden"}>
-          <div
-            className="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
-            role="alert"
-          >
-            <span className="font-medium">Info alert!</span> {message}
-          </div>
-        </div>
-      );
-    case "warning":
-      return (
-        <div className={show ? "block" : "hidden"}>
-          <div
-            className="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-400"
-            role="alert"
-          >
-            <span className="font-medium">Warning alert!</span> {message}
-          </div>
-        </div>
-      );
-    case "error":
-      return (
-        <div className={show ? "block" : "hidden"}>
-          <div
-            className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
-            role="alert"
-          >
-            <span className="font-medium">Error alert!</span> {message}
-          </div>
-        </div>
-      );
-    case "info":
-      return (
-        <div className={show ? "block" : "hidden"}>
-          <div
-            className="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
-            role="alert"
-          >
-            <span className="font-medium">Info alert!</span> {message}
-          </div>
-        </div>
-      );
-    default:
-      return null;
-  }
+
+  return (
+    <Stack
+      sx={{
+        width: "100%",
+        position: "absolute",
+        top: "100%",
+        left: "50%",
+        transform: "translate(-50%, -10%)",
+        zIndex: 1000,
+      }}
+      spacing={2}
+    >
+      <Alert
+        variant="filled"
+        severity={status}
+        sx={{ display: open ? "flex" : "none" }}
+      >
+        {message}
+      </Alert>
+    </Stack>
+  );
 };
+
+export default AlertComponent;
