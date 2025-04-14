a544984c89c5f980fef2b95ba0289682ee63e899
hooman <hoomanhajrian@gmail.com>
2025-04-13 17:58:14 -0700

    added material ui cache

--- src/app/providers/Providers.tsx
+++ src/app/providers/Providers.tsx
@@ -1,16 +1,20 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
-import { store } from "@/app/store";
+import { store } from "@/app/features/store";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
 interface ProviderProps {
   children: React.ReactNode;
+  pageProps?: { session: any };
 }
 
-function Providers({ children }: ProviderProps) {
+function Providers({ children, pageProps }: ProviderProps) {
   return (
-    <SessionProvider>
-      <Provider store={store}>{children}</Provider>
+    <SessionProvider session={pageProps?.session}>
+      <Provider store={store}>
+        <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+      </Provider>
     </SessionProvider>
   );
 }
