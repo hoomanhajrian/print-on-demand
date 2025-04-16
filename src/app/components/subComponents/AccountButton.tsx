505bc7a60d1dbd13521eb7f933dd22e199c69c1a
hooman <hoomanhajrian@gmail.com>
2025-04-15 20:46:10 -0700

    Account menu button and functions

---
+++ src/app/components/subComponents/AccountButton.tsx
@@ -0,0 +1,149 @@
+import { useState } from "react";
+import {
+  Box,
+  Avatar,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  Divider,
+  IconButton,
+  Typography,
+  Tooltip,
+} from "@mui/material";
+import ListIcon from "@mui/icons-material/List";
+import Settings from "@mui/icons-material/Settings";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Logout from "@mui/icons-material/Logout";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
+import { useDispatch } from "react-redux";
+import { clearUser } from "@/app/features/auth/userSlice";
+import { redirect } from "next/navigation";
+
+export const AccountButton = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const dispatch = useDispatch();
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <>
+      <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? "account-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+          >
+            <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        slotProps={{
+          paper: {
+            elevation: 0,
+            sx: {
+              overflow: "visible",
+              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+              mt: 1.5,
+              "& .MuiAvatar-root": {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              "&::before": {
+                content: '""',
+                display: "block",
+                position: "absolute",
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: "background.paper",
+                transform: "translateY(-50%) rotate(45deg)",
+                zIndex: 0,
+              },
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem
+          onClick={() => {
+            handleClose;
+            redirect("/main/profile");
+          }}
+        >
+          <ListItemIcon>
+            <Settings />
+          </ListItemIcon>
+          Profile
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <ListIcon fontSize="small" />
+          </ListItemIcon>
+          My Orderes
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <ListIcon fontSize="small" />
+          </ListItemIcon>
+          My Gigs
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <ShoppingCartIcon fontSize="small" />
+          </ListItemIcon>
+          Cart
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            handleClose;
+            dispatch(clearUser());
+            signOut({ callbackUrl: "/" });
+          }}
+        >
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+{
+  /* <Link href="/main/profile" className="text-gray-300 hover:text-white">
+            Profile
+          </Link>
+          
+          <button
+            type="button"
+            className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"
+            onClick={() => {
+              dispatch(clearUser());
+              signOut({ callbackUrl: "/" });
+            }}
+          >
+            Sign Out
+          </button> */
+}
