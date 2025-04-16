35cf9175756e36672560f6343dddf6c53134827f
hooman <hoomanhajrian@gmail.com>
2025-04-15 20:45:55 -0700

    responsive navbar

--- src/app/components/header/Navbar.tsx
+++ src/app/components/header/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
+import { useRef } from "react";
 import Link from "next/link";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useDispatch } from "react-redux";
 import { clearUser } from "@/app/features/auth/userSlice";
 import { useEffect } from "react";
@@ -8,9 +9,11 @@ import { useSelector } from "react-redux";
 import { getUser } from "@/app/features/auth/userSlice";
 import { setUserFromToken } from "@/app/features/auth/userSlice";
 import { redirect } from "next/navigation";
-
-export default function Navbar() {
-  const user = useSelector(getUser);
+import { AccountButton } from "@/app/components/subComponents/AccountButton";
+export const Navbar = () => {
+  const session = useSession();
+  const navRef = useRef<HTMLDivElement | null>(null);
+  const { data: user } = session;
   // Check if user is logged in
   if (!user) {
     redirect("/");
@@ -32,36 +35,91 @@ export default function Navbar() {
     };
   }, [user, dispatch]);
   return (
-    <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex items-center justify-between">
-        <Link href="/main" className="text-white font-bold text-xl">
-          Print-on-Demand
+    <nav className="bg-white border-gray-200 dark:bg-gray-900 flex flex-wrap items-center justify-between p-5">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto w-full p-4">
+        <Link
+          href="/main"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-8"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Print-on-Demand
+          </span>
         </Link>
-        <div className="flex space-x-4">
-          <Link href="/main" className="text-gray-300 hover:text-white">
-            Dashboard
-          </Link>
-          <Link href="/main/gigs" className="text-gray-300 hover:text-white">
-            Gigs
-          </Link>
-          <Link href="/main/orders" className="text-gray-300 hover:text-white">
-            Orders
-          </Link>
-          <Link href="/main/profile" className="text-gray-300 hover:text-white">
-            Profile
-          </Link>
-          <button
-            type="button"
-            className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"
-            onClick={() => {
-              dispatch(clearUser());
-              signOut({ callbackUrl: "/" });
-            }}
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+          onClick={() => {
+            const navbar = navRef.current;
+            if (navbar) {
+              navbar.classList.toggle("hidden");
+            }
+          }}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
           >
-            Sign Out
-          </button>
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+
+        <div className="hidden w-full md:block md:w-auto" ref={navRef}>
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border items-center border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <Link
+                href="/main"
+                className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                aria-current="page"
+                onClick={() => {
+                  const navbar = navRef.current;
+                  if (navbar) {
+                    navbar.classList.add("hidden");
+                  }
+                }}
+              >
+                Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/main/gigs"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                My Gigs
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/main/orders"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                My Orders
+              </Link>
+            </li>
+            <li>
+              <AccountButton />
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
   );
-}
+};
